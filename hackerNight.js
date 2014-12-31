1f597b02e05b3df75ae79c0836cb7cec6e799cfd
Vikash <vsahu24@gmail.com>
2014-12-30 20:53:49 -0500

    print stories in window (not just console)

--- hackerNight.js
+++ hackerNight.js
@@ -1,30 +1,35 @@
 
 
-//reference to top stories, version 0 of the HN api
-var HNRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
+function retrieve() {
+	//reference to top stories, version 0 of the HN api
+	var HNRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
+	var baseItemURL = "http://hacker-news.firebaseio.com/v0/item/";
+	
+	//item source
+	//"https://hacker-news.firebaseio.com/item/""
 
-var baseItemURL = "http://hacker-news.firebaseio.com/v0/item/";
-//item source
-//"https://hacker-news.firebaseio.com/item/""
+	HNRef.on("value", function(dataSnapshot) {
+		//dataSnapshot contains IDs of the top 100 stories
 
-HNRef.on("value", function(dataSnapshot) {
-	//dataSnapshot contains IDs of the top 100 stories
+		//for each story ID, retrieve corresponding object
+		dataSnapshot.forEach(function(childSnapshot) {
 
-	//for each story ID, retrieve corresponding object
-	dataSnapshot.forEach(function(childSnapshot) {
-
-		var itemString = baseItemURL + childSnapshot.val();
-		var ItemRef = new Firebase(itemString);
-		ItemRef.on('child_added', function(itemSnapshot) {
-			//need to display relevant information for each story, a la news.ycombinator.com
-			if (itemSnapshot.key() == "title") {
-				console.log(itemSnapshot.val());
-			}
+			var itemString = baseItemURL + childSnapshot.val();
+			var ItemRef = new Firebase(itemString);
+			ItemRef.on('child_added', function(itemSnapshot) {
+				//need to display relevant information for each story, a la news.ycombinator.com
+				if (itemSnapshot.key() == "title") {
+					console.log(itemSnapshot.val());
+					document.write(itemSnapshot.val().toString());
+					document.write("<br>");
+					//document.write(itemSnapshot.val());
+				}
+			});
 		});
-	});
 
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+	});	
+}
 
-}, function (errorObject) {
-	console.log("The read failed: " + errorObject.code);
-});
 
